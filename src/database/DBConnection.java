a7e4fde3216fde5ea445fa4b3c69708ef9950602
Edith Agai <email.02yt@gmail.com>
2026-02-24 01:55:29 +0300

    successfully connected to supabase cloud

--- src/database/DBConnection.java
+++ src/database/DBConnection.java
@@ -1,13 +1,49 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package database;
 
-/**
- *
- * @author edith
- */
-public class DBConnection {
+import java.sql.*;
+
+public /*abstract*/ class DBConnection {
+    protected String url = "jdbc:postgresql://aws-1-eu-central-1.pooler.supabase.com:5432/postgres?sslmode=require"; //sslmode=require is essential for cloud
+    //switched to shared pooler because of post 5432 block shared works better for ipv4  networks
+    protected String username = "postgres.tqiprmxpkdodjzgxyiyo";
+    protected String password = "ObjectOriented_2026"; 
+    public Connection con;
     
+    public DBConnection(){
+            //Step 1: Load Driver
+            try{
+                Class.forName("org.postgresql.Driver");
+                System.out.println("Driver loading sucessful");
+            }catch(ClassNotFoundException cnfe){
+                System.out.println("Driver Loading Failed"+ cnfe.getMessage());
+            }
+            //Step 2: Establish connection
+            try{
+                con = DriverManager.getConnection(url,username,password);
+                System.out.println("Connection sucessful");
+            }catch(SQLException sqle){
+                System.out.println("Conncetion failed: "+ sqle.getMessage());
+            }  
+    }
+    
+    public void closeConnection() {
+        try {
+            if (con != null && !con.isClosed()) {
+                con.close();
+                System.out.println("Database connection closed successfully.");
+            }
+        } catch (SQLException sqle) {
+            System.err.println("Error while closing connection: " + sqle.getMessage());
+        }
+    }
+
+    // Abstract methods as per assignment requirements
+    /*
+    public abstract void insert(int id, String name, String type, String email, String location, boolean status, int rating);
+    public abstract void update(int id, String name, String type, String email, String location, boolean status, int rating);
+    public abstract void delete(int id);
+    public abstract String[] select(int id);
+    public abstract void selectAll();
+    */
 }
+
